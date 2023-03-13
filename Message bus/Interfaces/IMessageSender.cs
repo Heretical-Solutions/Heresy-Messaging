29fc7df86568829b252add5e11ec5d303922738c
Benevolent Drone <benevolentdrone@gmail.com>
2023-03-13 09:05:39 +0200

    WIP updating message busses

--- Message bus/Interfaces/IMessageSender.cs
+++ Message bus/Interfaces/IMessageSender.cs
@@ -8,7 +8,7 @@ namespace HereticalSolutions.Messaging
         
         IMessageSender PopMessage(Type messageType, out IMessage message);
 
-        IMessageSender PopMessage<TMessage>(out IMessage message) where TMessage : IMessage;
+        IMessageSender PopMessage<TMessage>(out TMessage message) where TMessage : IMessage;
         
         #endregion
 
@@ -17,17 +17,19 @@ namespace HereticalSolutions.Messaging
         IMessageSender Write(IMessage message, object[] args);
         //IMessageSender Write(IMessage message, params object[] args);
         
+        IMessageSender Write<TMessage>(TMessage message, object[] args) where TMessage : IMessage;
+        
         #endregion
 
         #region Send
         
         void Send(IMessage message);
 
-        //void Send<TMessage>(IMessage message) where TMessage : IMessage;
+        void Send<TMessage>(TMessage message) where TMessage : IMessage;
 
         void SendImmediately(IMessage message);
 
-        //void SendImmediately<TMessage>(IMessage message) where TMessage : IMessage;
+        void SendImmediately<TMessage>(TMessage message) where TMessage : IMessage;
         
         #endregion
 
