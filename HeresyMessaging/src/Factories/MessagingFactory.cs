e90900331d0344a00d2f0278b9153d8ed87836f5
Benevolent Drone <benevolentdrone@gmail.com>
2023-03-13 10:24:52 +0200

    Rewritten non alloc message bus

--- HeresyMessaging/src/Factories/MessagingFactory.cs
+++ HeresyMessaging/src/Factories/MessagingFactory.cs
@@ -1,13 +1,3 @@
-using System;
-using HereticalSolutions.Collections;
-using HereticalSolutions.Collections.Allocations;
-
-using HereticalSolutions.Collections.Factories;
-using HereticalSolutions.Messaging.Broadcasting;
-using HereticalSolutions.Messaging.Pinging;
-using HereticalSolutions.Pools;
-using HereticalSolutions.Pools.Factories;
-
 namespace HereticalSolutions.Messaging.Factories
 {
 	public static partial class MessagingFactory
